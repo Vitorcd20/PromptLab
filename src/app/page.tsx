2755d7daeb05af6a568e9b036816921eaf4d04df
Vitorcd20 <vitorvieeira12008@gmail.com>
2026-02-04 20:50:43 -0300

    feat: create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 export default function Home() {
   return (
-    <div>
-      <h2>prompt</h2>
-    </div>
+    <section className="flex min-h-full">
+      <div className="flex flex-1 items-center justify-center">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold mb-4">Select a prompt</h1>
+          <p className="text-gray-400">
+            Choose a prompt from the list on the side to view and edit
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
