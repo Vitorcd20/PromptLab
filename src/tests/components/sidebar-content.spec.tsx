1a6d1a0a94e3816507ddeb1e430dc26873b40ab1
Vitorcd20 <vitorvieeira12008@gmail.com>
2026-02-06 11:31:24 -0300

    feat: create sidebar test

---
+++ src/tests/components/sidebar-content.spec.tsx
@@ -0,0 +1,17 @@
+import SidebarContent from '@/components/sidebar/sidebar-content';
+import { render, screen } from '@/lib/test-utils';
+
+jest.mock('next/navigation', () => ({
+  useRouter: () => ({ push: jest.fn() }),
+}));
+
+describe('SidebarContent', () => {
+  it('should render a button to create a new prompt', () => {
+    render(<SidebarContent />);
+
+    expect(screen.getByRole('complementary')).toBeVisible();
+    expect(
+      screen.getByRole('button', { name: 'New Prompt' })
+    ).toBeInTheDocument();
+  });
+});
